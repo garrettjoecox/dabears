63b20a84d3172ee915d713ac5511fa6c8b33a4e9
Garrett Cox <garrettjcox@gmail.com>
2020-02-07 09:53:57 -0600

    deploy

---
+++ precache-manifest.189fe0c67c2008ac0a2a48e7ad955381.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8319450a15697271b0",
+    "url": "/dabears/css/app.7cb63e93.css"
+  },
+  {
+    "revision": "62f967c871f3756cbb3b5dbec8431b39",
+    "url": "/dabears/img/107.62f967c8.jpg"
+  },
+  {
+    "revision": "39f97d9fb785b50b796aee41747657bf",
+    "url": "/dabears/img/131.39f97d9f.jpg"
+  },
+  {
+    "revision": "fc8c3af8728e2168c58cacbb6c25134d",
+    "url": "/dabears/img/203.fc8c3af8.jpg"
+  },
+  {
+    "revision": "780745286c72c47ba079b3461671a882",
+    "url": "/dabears/img/34.78074528.jpg"
+  },
+  {
+    "revision": "9939dda56946dcf3acf6646530c9ab9a",
+    "url": "/dabears/img/39.9939dda5.jpg"
+  },
+  {
+    "revision": "bf5fae0a7f5cffee8a3be766b46ab847",
+    "url": "/dabears/img/74.bf5fae0a.jpg"
+  },
+  {
+    "revision": "9bb71fee0b33776163c8b655bcf072a9",
+    "url": "/dabears/img/78.9bb71fee.jpg"
+  },
+  {
+    "revision": "0ee68dc6135d55359e692adecd17fb20",
+    "url": "/dabears/img/afterhours.0ee68dc6.jpg"
+  },
+  {
+    "revision": "63e79c9ea9cb31a4fee7455496a0168a",
+    "url": "/dabears/img/amnesia.63e79c9e.jpg"
+  },
+  {
+    "revision": "475a6a307a53d16f6486011ef4c58930",
+    "url": "/dabears/img/bumpin-the-night.475a6a30.jpg"
+  },
+  {
+    "revision": "4f0e182355a2c22f19df278042d4f0d5",
+    "url": "/dabears/img/crank.4f0e1823.jpg"
+  },
+  {
+    "revision": "f78a6f35035398368bae6c5e6fe45787",
+    "url": "/dabears/img/dub-till-dawn.f78a6f35.jpg"
+  },
+  {
+    "revision": "52462182d6d2bf969bd4d035a008ce65",
+    "url": "/dabears/img/dublicious.52462182.jpg"
+  },
+  {
+    "revision": "63f2a1563ea8dd1b3daa96f95850c5c9",
+    "url": "/dabears/img/elixir-3.63f2a156.jpg"
+  },
+  {
+    "revision": "ea609513edd97f8fdd512da972280e6e",
+    "url": "/dabears/img/eloquence.ea609513.jpg"
+  },
+  {
+    "revision": "714b641203c932caba9757353431d216",
+    "url": "/dabears/img/embers.714b6412.jpg"
+  },
+  {
+    "revision": "c93a6c29058f2a6d526629931c93adbc",
+    "url": "/dabears/img/euphoria.c93a6c29.jpg"
+  },
+  {
+    "revision": "f1b7953a99d3c7ee296e561a0264ca8c",
+    "url": "/dabears/img/evanescent.f1b7953a.jpg"
+  },
+  {
+    "revision": "5c72e3cfd4ce75724ddbdd96d2b172cd",
+    "url": "/dabears/img/flow.5c72e3cf.jpg"
+  },
+  {
+    "revision": "932e25721a2bbbb93575319de485a4e1",
+    "url": "/dabears/img/frequency.932e2572.jpg"
+  },
+  {
+    "revision": "b01eba00b6777680252e192a70f2702a",
+    "url": "/dabears/img/from-dusk-till-dawn.b01eba00.jpg"
+  },
+  {
+    "revision": "8cef43503b0c9fb053ea4e4889ad280c",
+    "url": "/dabears/img/hello-world.8cef4350.jpg"
+  },
+  {
+    "revision": "24383c8247865eb22f12bb379a80e25a",
+    "url": "/dabears/img/house-nights.24383c82.jpg"
+  },
+  {
+    "revision": "c20a8dfd4312d5b58e8b9d16453db195",
+    "url": "/dabears/img/i-2.c20a8dfd.jpg"
+  },
+  {
+    "revision": "3d8fc2d6467803ad0c2fe6227f1d2214",
+    "url": "/dabears/img/i-3.3d8fc2d6.jpg"
+  },
+  {
+    "revision": "583290854644a0040ca8303edabd65ed",
+    "url": "/dabears/img/i-4.58329085.jpg"
+  },
+  {
+    "revision": "1716fc11fde6f7f812ddb0c746777956",
+    "url": "/dabears/img/i-5.1716fc11.jpg"
+  },
+  {
+    "revision": "51f905ecf89a1d0f653178cdcfe0d83f",
+    "url": "/dabears/img/i-6.51f905ec.jpg"
+  },
+  {
+    "revision": "dcf3fd331c45e4ec21afc77cbecd81a1",
+    "url": "/dabears/img/i.dcf3fd33.jpg"
+  },
+  {
+    "revision": "c679458e60809985bd6e3a2df6c80735",
+    "url": "/dabears/img/in-too-deep.c679458e.jpg"
+  },
+  {
+    "revision": "86d16a5a227e03bc97cea52256b4cd7a",
+    "url": "/dabears/img/its-a-trap.86d16a5a.jpg"
+  },
+  {
+    "revision": "90b37f9cffc3d29c8345fc188cf9f415",
+    "url": "/dabears/img/lost.90b37f9c.jpg"
+  },
+  {
+    "revision": "332482a6127ebd9b00e1d85b13967195",
+    "url": "/dabears/img/paralyzer.332482a6.jpg"
+  },
+  {
+    "revision": "8efbe11e93a1c79c116b5bc7aaa969fd",
+    "url": "/dabears/img/peace-love-dub.8efbe11e.jpg"
+  },
+  {
+    "revision": "fc0265cbdecea33e06e5174ee33da781",
+    "url": "/dabears/img/pulse.fc0265cb.jpg"
+  },
+  {
+    "revision": "1bae06e628ce5d8dcece440d0a034d9e",
+    "url": "/dabears/img/punch.1bae06e6.jpg"
+  },
+  {
+    "revision": "56b0b5014f41f63784d70d408a0c78dd",
+    "url": "/dabears/img/serene.56b0b501.jpg"
+  },
+  {
+    "revision": "63fb4943821c1142ce4f13c7e9eca2fb",
+    "url": "/dabears/img/serenity.63fb4943.jpg"
+  },
+  {
+    "revision": "674d54d4eebeb8d6b1389de081361cd6",
+    "url": "/dabears/img/shimmers.674d54d4.jpg"
+  },
+  {
+    "revision": "a62b0d726317044b6f1329a8ed383554",
+    "url": "/dabears/img/sick-tonight.a62b0d72.jpg"
+  },
+  {
+    "revision": "c8843a80539d15cbdd3a21722c050acf",
+    "url": "/dabears/img/sunrise.c8843a80.jpg"
+  },
+  {
+    "revision": "0c6abef7ca80473bd9b452a3a6c58f10",
+    "url": "/dabears/img/surrender.0c6abef7.jpg"
+  },
+  {
+    "revision": "818a97b4208b5bbeb419db21e43fa751",
+    "url": "/dabears/img/thats-tasty.818a97b4.jpg"
+  },
+  {
+    "revision": "319a08960bf0bbe891d36103a7cb710c",
+    "url": "/dabears/img/tranquility.319a0896.jpg"
+  },
+  {
+    "revision": "abd454688005ceb2a3948f3323b4ea9a",
+    "url": "/dabears/img/transcendence.abd45468.jpg"
+  },
+  {
+    "revision": "4c49f1756128114ccc522a1b5c5b1819",
+    "url": "/dabears/img/turbulence.4c49f175.jpg"
+  },
+  {
+    "revision": "508db9f1a819dcde7d14cf0756cf5a2e",
+    "url": "/dabears/img/twist.508db9f1.jpg"
+  },
+  {
+    "revision": "dcaae46854756f4360c74522fe428bc9",
+    "url": "/dabears/img/visions.dcaae468.jpg"
+  },
+  {
+    "revision": "5c12a7c501d1cb38e57859360c3b7a0e",
+    "url": "/dabears/img/welcome-to-the-weekend.5c12a7c5.jpg"
+  },
+  {
+    "revision": "494266b0ca0fbf3e6f9f4635d03b32b5",
+    "url": "/dabears/img/wonderland.494266b0.jpg"
+  },
+  {
+    "revision": "3576d85ad2920ad98f289618369af72b",
+    "url": "/dabears/index.html"
+  },
+  {
+    "revision": "6f8319450a15697271b0",
+    "url": "/dabears/js/app.0b9a0434.js"
+  },
+  {
+    "revision": "1a8936160623ed17afb5",
+    "url": "/dabears/js/chunk-vendors.986d4b5d.js"
+  },
+  {
+    "revision": "1678fd2d39036427e9841734b58e93a0",
+    "url": "/dabears/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dabears/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dabears/precache-manifest.189fe0c67c2008ac0a2a48e7ad955381.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "dabears"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
