1dbcd9641d9c53ce47770e9aa5e26b085a8304eb
Garrett Cox <garrettjcox@gmail.com>
2020-12-20 10:30:01 -0600

    Env setup

---
+++ src/client/theme/index.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+
+export default extendTheme({
+  styles: {
+    global: (props) => ({
+      'html, body, #root': {
+        height: '100%',
+      },
+      body: {
+        fontFamily: 'body',
+        color: mode('gray.800', 'whiteAlpha.900')(props),
+        bg: mode('white', 'gray.900')(props),
+        transition: 'background-color 0.2s',
+        lineHeight: 'base',
+      },
+      '*::placeholder': {
+        color: mode('gray.400', 'whiteAlpha.400')(props),
+      },
+      '*, *::before, &::after': {
+        borderColor: mode('gray.200', 'whiteAlpha.300')(props),
+        wordWrap: 'break-word',
+      },
+    }),
+  },
+  config: {
+    useSystemColorMode: false,
+    initialColorMode: 'dark',
+  },
+});
